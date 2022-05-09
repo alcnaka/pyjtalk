1213ebdf98fb67493723acb5719964111cbc2889
alcnaka <47954083+alcnaka@users.noreply.github.com>
2022-05-10 03:24:18 +0900

    change path

--- pyjtalk/config.py
+++ pyjtalk/config.py
@@ -5,7 +5,7 @@ logger = logging.getLogger(__name__)
 
 
 PYJTALK_DEFAULT_VOICE_PATH = os.getenv("PYJTALK_DEFAULT_VOICE_PATH") \
-    or "/etc/pyjtalk/voice/mei/mei_normal.htsvoice"
+    or "/usr/share/pyjtalk/voice/mei/mei_normal.htsvoice"
 
 PYJTALK_DEFAULT_DICTIONARY_PATH = os.getenv("PYJTALK_DEFAULT_DICTIONARY_PATH") \
     or "/var/lib/mecab/dic/open-jtalk/naist-jdic/"
